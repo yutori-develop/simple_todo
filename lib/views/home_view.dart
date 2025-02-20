8279892e777b173a56bb77cf49634011a1088869
松崎祐太 <yuta@matsuzakiyuutanoiMac.local>
2025-02-20 15:21:59 +0900

    todoモデルの定義完了

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:intl/intl.dart';
 import 'package:simple_todo/gen/assets.gen.dart';
+import 'package:simple_todo/data/models/todo.dart';
+import 'package:uuid/uuid.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -12,6 +14,8 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     Intl.defaultLocale = Localizations.localeOf(context).toString();
     const envValue = String.fromEnvironment('testValue');
+    final testTodo = Todo(id: Uuid().v4(), title: 'これはテストTodoです');
+
     return Scaffold(
       appBar: AppBar(
         title: Text(L10n.of(context).helloWorld),
@@ -28,6 +32,7 @@ class HomeView extends StatelessWidget {
             Assets.images.testPng.image(),
             Assets.images.testSvg.svg(width: 100, height: 100),
             Text('環境変数テスト: $envValue'),
+            Text('TestTodoのidは${testTodo.id}で、titleは${testTodo.title}です'),
           ],
         ),
       ),
