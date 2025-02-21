473393ce768d25b4a20d768a4556611b8aab0500
松崎祐太 <yuta@matsuzakiyuutanoiMac.local>
2025-02-21 12:42:53 +0900

    HomeViewをConsumerStatefulWidgetに変更

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -3,38 +3,40 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'package:simple_todo/view_models/todos_view_model.dart';
 
-class HomeView extends ConsumerWidget {
+class HomeView extends ConsumerStatefulWidget {
   const HomeView({super.key});
 
   static const path = '/';
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final todos = ref.watch(todosViewModelProvider);
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomeViewState();
+}
 
+class _HomeViewState extends ConsumerState<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    final todos = ref.watch(todosViewModelProvider);
     return Scaffold(
       appBar: AppBar(
         title: Text('${DateFormat.yMd().format(DateTime.now())}にやること'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: Expanded(
-        child: ListView.builder(
-          itemBuilder: ((context, index) {
-            final todo = todos[index];
+      body: ListView.builder(
+        itemBuilder: ((context, index) {
+          final todo = todos[index];
 
-            return ListTile(
-              title: Text(todo.taskName),
-              trailing: Checkbox(
-                  value: todo.isCompleted,
-                  onChanged: (newValue) {
-                    ref
-                        .read(todosViewModelProvider.notifier)
-                        .toggleStatus(todo.id, newValue ?? false);
-                  }),
-            );
-          }),
-          itemCount: todos.length,
-        ),
+          return ListTile(
+            title: Text(todo.taskName),
+            trailing: Checkbox(
+                value: todo.isCompleted,
+                onChanged: (newValue) {
+                  ref
+                      .read(todosViewModelProvider.notifier)
+                      .toggleStatus(todo.id, newValue ?? false);
+                }),
+          );
+        }),
+        itemCount: todos.length,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
